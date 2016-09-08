7ab863d6edc4007694a8746a9ddab0365db00f2d
Grant McNeil <grant.mcneil@gmail.com>
2016-09-08 15:28:02 -0400

    fog of war test

--- twentysixteen/src/techdemos/fogofwartechdemo.h
+++ twentysixteen/src/techdemos/fogofwartechdemo.h
@@ -2,6 +2,14 @@
 
 #include "basetechdemo.h"
 #include "../spine_data.h"
+#include "../linear_algebra.h"
+
+typedef struct
+{
+	int wall;
+	std::vector<std::pair<int, int>> visible_tiles;
+	bool visible;
+}t_tile;
 
 class FogOfWarTechDemo : public BaseTechDemo
 {
@@ -15,8 +23,16 @@ public:
 	void draw();
 	void init();
 	void take_input(boundinput input, bool type);
+	void compute_visibility(int i, int j);
 
-	std::map<int, std::map<int, int>> tile_map;
+	int x, y;
+
+	std::map<int, std::map<int, t_tile>> tile_map;
+
+
+	t_polygon get_vision_rect(int i, int j, float i2, float j2);
+
+	bool check_collision(t_polygon rect, int i, int j);
 
 	// SOIL test
 	SpineData spineboy;
